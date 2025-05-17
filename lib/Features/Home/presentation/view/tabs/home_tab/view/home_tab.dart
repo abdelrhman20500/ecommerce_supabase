6d351844d92dc39230fad0640c5990199c7e5740
AbdElrhman AbdElfattah <abdelrhmanabdelfattah9866@gmail.com>
2025-05-17 19:12:44 +0200

    Create Home Search Bar And Adding Image

--- lib/Features/Home/presentation/view/tabs/home_tab/view/home_tab.dart
+++ lib/Features/Home/presentation/view/tabs/home_tab/view/home_tab.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
+import '../../../widget/custom_search_field.dart';
 
 class HomeTab extends StatelessWidget {
   const HomeTab({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.green,);
+    var height= MediaQuery.of(context).size.height;
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: ListView(
+        children: [
+          const CustomSearchField(),
+          SizedBox(height: height*0.03,),
+          Image.asset("assets/images/buy.jpg")
+        ],
+      ),
+    );
   }
 }
+
---
+++ lib/Features/Home/presentation/view/widget/custom_search_field.dart
@@ -0,0 +1,48 @@
+import 'package:ecommerce_supabase/Core/utilis/app_color.dart';
+import 'package:flutter/material.dart';
+
+class CustomSearchField extends StatelessWidget {
+  const CustomSearchField({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      keyboardType: TextInputType.emailAddress,
+      decoration:InputDecoration(
+        suffixIcon:ElevatedButton.icon(
+            style: ElevatedButton.styleFrom(
+                backgroundColor: AppColors.kPrimaryColor,
+                foregroundColor: AppColors.kWhiteColor,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8)
+                )
+            ),
+            onPressed: (){}, label: Icon(Icons.search)) ,
+        labelText: "Search in Market",
+        border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8),
+            borderSide: const BorderSide(
+                width: 2,
+                color: AppColors.kBordersideColor
+            )
+        ),
+        enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8),
+            borderSide: const BorderSide(
+                width: 2,
+                color: AppColors.kBordersideColor
+            )
+        ),
+        focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8),
+            borderSide: const BorderSide(
+                width: 2,
+                color: AppColors.kBordersideColor
+            )
+        ),
+      ),
+    );
+  }
+}
